ee0f81e7f7b3b81f9e78cdab43721f1e6336f209
ThePerformanceConsultant <will@theperformanceconsultant.net>
2025-11-30 13:17:47 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,6 +6,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
+    minify: true,
   },
 });
